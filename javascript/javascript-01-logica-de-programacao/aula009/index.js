c6cc4175a1a7b8125d53783848847f9a7437b1b1
Gustavo Ferreira <gustavoferreira.developer@gmail.com>
2023-01-04 06:44:25 -0300

    Finalizando as aulas do primeiro módulo antes do primeiro exercício

---
+++ javascript/javascript-01-logica-de-programacao/aula009/index.js
@@ -0,0 +1,4 @@
+let name = "Gustavo Ferreira";
+let x = 5;
+console.log(name);
+console.log(x);
---
+++ javascript/javascript-01-logica-de-programacao/aula010/index.js
@@ -0,0 +1,9 @@
+let nome = "Gustavo";
+
+console.log(nome);
+console.error(nome);
+console.warn(nome);
+
+//window.alert(nome);
+//nome = window.prompt("Insira o seu nome: ");
+//let confirmacao = window.confirm("Confirma essa ação?");
